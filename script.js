864b82456a50b07d6fbb1022f42d068a1c238e86
Ferdinand Engländer <ferdinand.englaender@concepts-and-training.de>
2024-07-10 18:59:09 +0200

    visual select prototype

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ class SearchableList {
      * @param {string} containerID - the ID of the container with the search input and the list
      */
     constructor(containerID) {
+        // basic class names
         this.searchbarInput = ".c-searchable-list__searchbar input";
         this.componentBody = ".c-searchable-list__body";
         this.list = ".c-searchable-list__list";
         this.listItem = ".c-searchable-list__item";
 
-        const ErrorHintMissingClass = 'Make sure HTML elements have the expected class names.'
+        this.containerID = containerID;
 
-        // Check if all DOM elements exist: container, search bar, component body, list to be filtered
+        if (this.constructor === SearchableList) {
+            this.init();
+        }
+    }
+
+    init() {
+        const ErrorHintMissingClass = 'Make sure HTML elements have the expected class names.';
+        // Check if all necessary DOM elements exist
         try {
-            this.searchContainer = document.getElementById(containerID);
+            this.searchContainer = document.getElementById(this.containerID);
             if (this.searchContainer == null) {
                 throw new ReferenceError(`Container element not found or ID not unique. ${ErrorHintMissingClass}`);
             }
@@ -24,24 +32,35 @@ class SearchableList {
                 { name: "list", selector: this.list }
             ];
 
-            htmlElementsToFetch.forEach(element => {
-                this[element.name] = this.searchContainer.querySelector(element.selector);
-                if (this[element.name] === null) {
-                    throw new ReferenceError(`${element.name} element not found. ${ErrorHintMissingClass}`);
-                }
-            });
+            this.fetchElements();
 
             // bind the input bar so any input filters the list
             this.filterItemsSearch = this.filterItemsSearch.bind(this);
             this.searchbarInput.addEventListener('input', this.filterItemsSearch);
 
-            this.selectableItems = Array.from(this.list.querySelectorAll(this.listItem));
+            this.selectableItems = Array.from(this.searchContainer.querySelectorAll(this.listItem));
         } catch (e) {
             console.error(e);
         }
     }
 
-    // methods for main class and all subclasses
+    fetchElements() {
+        const ErrorHintMissingClass = 'Make sure HTML elements have the expected class names.';
+
+        const htmlElementsToFetch = [
+            { name: "searchbarInput", selector: this.searchbarInput },
+            { name: "componentBody", selector: this.componentBody },
+            { name: "list", selector: this.list }
+        ];
+
+        htmlElementsToFetch.forEach(element => {
+            this[element.name] = this.searchContainer.querySelector(element.selector);
+            if (this[element.name] === null) {
+                throw new ReferenceError(`${element.name} element not found. ${ErrorHintMissingClass}`);
+            }
+        });
+    }
+
     filterItemsSearch(event) {
         const value = event.target.value.toLowerCase();
         this.selectableItems.forEach(container => {
@@ -69,7 +88,10 @@ class SearchableCheckableList extends SearchableList {
         super(containerID);
         this.listItemCheckbox = 'input';
         this.checkedItemClass = 'selected';
-        this.handleSelection(this.listItemCheckbox, resetAllOthers);
+        if (this.constructor === SearchableCheckableList) {
+            this.init();
+            this.handleSelection(this.listItemCheckbox, resetAllOthers);
+        }
     }
 
     handleSelection = (checkboxOrRadioInput, resetAllOthers) => {
@@ -101,7 +123,25 @@ class SearchableCheckableList extends SearchableList {
     };
 }
 
+class VisualSelect extends SearchableCheckableList {
+    constructor(containerID, resetAllOthers = false) {
+        super(containerID, resetAllOthers);
+        this.searchbarInput = ".c-visual-select__searchbar input";
+        this.componentBody = ".c-visual-select__body";
+        this.list = ".c-visual-select__list";
+        this.listItem = ".c-visual-select__item";
+
+        this.init(); // Call init after setting subclass-specific properties
+        this.handleSelection(this.listItemCheckbox, resetAllOthers);
+    }
+
+    fetchElements() {
+        super.fetchElements();
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     new SearchableCheckableList('searchableMultiList', false);
     new SearchableCheckableList('searchableSingleList', true);
+    new VisualSelect('visualSelect', true);
 });
